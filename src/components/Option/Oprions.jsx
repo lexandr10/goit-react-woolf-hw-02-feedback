95b7de131312dcd8fb7d5e03770a9735edb8e860
lexandr10 <alexandrenko3006@gmail.com>
2024-02-18 11:08:13 -0500

    change name and fix function

--- src/components/Option/Oprions.jsx
+++ src/components/Option/Oprions.jsx
@@ -2,13 +2,13 @@ import stl from './option.module.css';
 const Option = ({ options, onLeaveFeedback }) => {
   return (
     <div className={stl.block}>
-      {Object.keys(options).map(option => (
+      {options.map(option => (
         <button
           className={stl.btn}
           key={option}
           type="button"
           name={option}
-          onClick={onLeaveFeedback}
+          onClick={() => onLeaveFeedback(option)}
         >
           {option}
         </button>
